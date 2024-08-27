37553c31041b3873200547b6e8f78fc03dd27abf
jm2359 <jebinm98@gmail.com>
2024-08-27 01:05:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showGallery() {
+    // Hide the home container
+    document.getElementById('home-container').style.display = 'none';
+    
+    // Show the gallery container
+    document.getElementById('gallery-container').style.display = 'block';
+}
